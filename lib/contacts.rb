10a6a3ea092f9c68e37ca6c55dce6a810160320b
Amgad Malik <amgadmalik@yahoo.com>
2019-09-24 01:50:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,5 +16,5 @@ require 'pry'
   
 def remove_strawberry(contacts)
   binding.pry
-  contacts["freddy mercury"][favorite_ice_cream_flavors].shift
+  contacts["Freddy mercury"][favorite_ice_cream_flavors].shift
 end
